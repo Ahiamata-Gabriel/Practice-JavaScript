b5fac02e9dd84adf6ff383d454231d8b7faeea67
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-07 01:44:01 +0000

    Introduction to the complete Javascript coursse

---
+++ The Complete JavaScript Course/0.Introduction/script.js
@@ -0,0 +1,11 @@
+const jonasArray = [
+  "Jonas",
+  "schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Micheal", "Peter", "Steven"],
+];
+
+for (let i = 0; i < jonasArray.length; i++) {
+  console.log(jonasArray[i]);
+}
