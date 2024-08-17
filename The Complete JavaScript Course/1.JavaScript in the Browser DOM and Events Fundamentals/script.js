9c730ba1d3e2c9ec86379360c846e6eb1277f077
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-17 22:51:19 +0000

    update

--- The Complete JavaScript Course/1.JavaScript in the Browser DOM and Events Fundamentals/script.js
+++ The Complete JavaScript Course/1.JavaScript in the Browser DOM and Events Fundamentals/script.js
@@ -10,11 +10,16 @@ document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  //When there is no input
   if (!guess) {
     message.textContent = "Please enter a number😁";
+
+    //when guess is correct
   } else if (guess === secretNumber) {
     message.textContent = "Correct Number";
     score++;
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       message.textContent = "Number too high😒";
@@ -24,6 +29,8 @@ document.querySelector(".check").addEventListener("click", function () {
       message.textContent = "You loose";
       scoreEl.textContent = 0;
     }
+
+    //When guess is to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       message.textContent = "Number too low😒";
