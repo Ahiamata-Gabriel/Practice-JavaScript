16ef9b75a6243063e2a44d2314f6243197073025
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-24 22:25:47 +0000

    script.js

--- The Complete JavaScript Course/0.Introduction/script.js
+++ The Complete JavaScript Course/0.Introduction/script.js
@@ -1,11 +1,14 @@
-const jonasArray = [
-  "Jonas",
-  "schmedtmann",
-  2037 - 1991,
-  "teacher",
-  ["Micheal", "Peter", "Steven"],
-];
+"use strict";
+const printForestcast = (array) => {
+  if (array.length === 0) return;
+  let results = "";
 
-for (let i = 0; i < jonasArray.length; i++) {
-  console.log(jonasArray[i]);
-}
+  for (let i = 0; i < array.length; i++) {
+    let currentArray = array[i];
+    results += `... ${currentArray}°C in ${i + 1} days `;
+  }
+  return results;
+};
+
+console.log(printForestcast([17, 21, 23]));
+console.log(printForestcast([12, 5, -5, 0, 4]));
