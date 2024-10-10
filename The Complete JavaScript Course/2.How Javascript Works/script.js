da9c0ac23954de60e5893d5a69092bb5a03e3b7c
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-10-10 16:56:51 +0000

    update

--- The Complete JavaScript Course/2.How Javascript Works/script.js
+++ The Complete JavaScript Course/2.How Javascript Works/script.js
@@ -1,17 +1,11 @@
-"use strict";
+const jonas = {
+  firstName: "John",
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-
-  function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
-    console.log(output);
-  }
-
-  printAge();
-
-  return age;
-}
-
-const firstName = "Jonas";
-calcAge(1991);
+jonas.greet();
