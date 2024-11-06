8429117a0e737a7e27cb3f8dc301060168d05e39
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-11-06 16:56:06 +0000

    update

--- The Complete JavaScript Course/4. A Closer Look at Functions/script.js
+++ The Complete JavaScript Course/4. A Closer Look at Functions/script.js
@@ -1,9 +1,16 @@
 "use strict";
 
+const bookings = [];
+
 const createBooking = function (flightNum, numPassengers, price) {
   const booking = {
     flightNum,
     numPassengers,
     price,
   };
+
+  console.log(booking);
+  bookings.push(booking);
 };
+
+createBooking("H258", 19, 2300);
