24e7759f203ff24cc3ca0a25766c310c0e40434d
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-11-18 11:33:45 +0000

    update

--- The Complete JavaScript Course/4. A Closer Look at Functions/script.js
+++ The Complete JavaScript Course/4. A Closer Look at Functions/script.js
@@ -15,12 +15,14 @@
 
 // createBooking("H258", 19, 2300);
 
-const flight = "mn527";
-const gabby = {
-  name: "Ahiamatski GAbby",
-  passport: 259689,
-};
+// const flight = "mn527";
+// const gabby = {
+//   name: "Ahiamatski GAbby",
+//   passport: 259689,
+// };
 
-const checkIn = function (flightNum, passenger) {};
+// const checkIn = function (flightNum, passenger) {};
 
-checkIn(flight, gabby);
+// checkIn(flight, gabby);
+
+const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
