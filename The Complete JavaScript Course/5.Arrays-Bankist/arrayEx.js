f073a33c1692a3c45116f33a8ae57216aa8832d2
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-12-23 16:36:24 +0000

    update

--- The Complete JavaScript Course/5.Arrays-Bankist/arrayEx.js
+++ The Complete JavaScript Course/5.Arrays-Bankist/arrayEx.js
@@ -1,7 +1,21 @@
-// const user = "Steven Thomas Williams";
-// const username = user
-//   .toLowerCase()
-//   .split(" ")
-//   .map((user) => user[0])
-//   .join("");
-// console.log(username);
+const calcAverageHumanAge = (ages) => {
+  let allAumanAge = [];
+  for (const age of ages) {
+    if (age <= 2) {
+      const humanAge = 2 * age;
+      allAumanAge.push(humanAge);
+    } else {
+      humanAge = 16 + age * 4;
+      allAumanAge.push(humanAge);
+      //console.log(allAumanAge);
+    }
+  }
+
+  const above18 = allAumanAge.filter((age) => age > 18);
+  const average = above18.reduce((total, age) => (total + age) / 2);
+
+  return average;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 1, 15, 8, 3]));
