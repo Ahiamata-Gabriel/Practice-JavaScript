bfbf54927c0c188516b46f55d83c3c5aa075d32f
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-01-10 05:21:48 +0000

    update

--- The Complete JavaScript Course/5.Arrays-Bankist/script.js
+++ The Complete JavaScript Course/5.Arrays-Bankist/script.js
@@ -179,6 +179,12 @@ btnClose.addEventListener("click", function (e, currentAccount) {
     inputCloseUsername.value === currentAccount.username &&
     Number(inputClosePin.value) === currentAccount.pin
   ) {
+    const index = accounts.findIndex(
+      (acc) => (acc.username = currentAccount.username)
+    );
+
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
   }
 });
 
