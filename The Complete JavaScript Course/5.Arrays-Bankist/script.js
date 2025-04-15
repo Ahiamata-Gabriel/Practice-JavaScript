05593b40ed5a90b4464816964a43d60dcfe62985
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-04-15 04:20:58 +0000

    update

--- The Complete JavaScript Course/5.Arrays-Bankist/script.js
+++ The Complete JavaScript Course/5.Arrays-Bankist/script.js
@@ -79,9 +79,12 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const displayMovements = function (movements) {
   movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
     const html = `
     <div class="movements__row">
-          <div class="movements__type movements__type--withdrawal"> ${i + 1}
+          <div class="movements__type movements__type--${type}">
+           ${i + 1} ${type}
           </div>
           <div class="movements__value">${mov}</div>
         </div> 
