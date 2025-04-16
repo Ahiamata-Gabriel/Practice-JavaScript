be422b91297ab8db2b96ac9acdffb776a0b92b0b
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-04-16 04:05:36 +0000

    update

--- The Complete JavaScript Course/5.Arrays-Bankist/script.js
+++ The Complete JavaScript Course/5.Arrays-Bankist/script.js
@@ -109,3 +109,13 @@ const movementsDescriptions = movements.map((mov, i) => {
   }
 });
 console.log(movementsDescriptions);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
