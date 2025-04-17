3be978a922a01192433988f52d4968c8f8dbbc5a
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-04-17 23:09:02 +0000

    update

--- The Complete JavaScript Course/5.Arrays-Bankist/script.js
+++ The Complete JavaScript Course/5.Arrays-Bankist/script.js
@@ -88,7 +88,7 @@ const displayMovements = function (movements) {
           <div class="movements__type movements__type--${type}">
            ${i + 1} ${type}
           </div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
         </div> 
     `;
 
@@ -124,10 +124,17 @@ const withdrawals = movements.filter((mov) => mov < 0);
 
 const calDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance} €`;
 };
 
-const totalDepositsUSD = movements
-  .filter((mov) => mov > 0)
-  .map(mov * eurToUSD)
-  .reduce((acc, mov) => acc + mov, 0);
+calDisplayBalance(account1.movements);
+
+const calcDispalySummary = (movements) => {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumIn.textContent = `${incomes}€`;
+};
+
+calcDispalySummary(account1.movements);
