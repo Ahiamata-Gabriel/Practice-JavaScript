96b9e6496718fbad9cd5ee4bab705fcf53e7a8cb
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-05-14 02:20:06 +0000

    update

--- The Complete JavaScript Course/5.Arrays-Bankist/script.js
+++ The Complete JavaScript Course/5.Arrays-Bankist/script.js
@@ -203,6 +203,14 @@ btnTransfer.addEventListener("click", function (e) {
 btnLoan.addEventListener("click", (e) => {
   e.preventDefault();
   const amount = Number(inputLoanAmount.value);
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
 });
 
 btnClose.addEventListener("click", (e) => {
