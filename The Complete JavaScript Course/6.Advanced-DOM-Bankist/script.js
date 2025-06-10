9d65fa4d6990823b6b22ec036e0f3a86b08be5ce
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-06-10 05:27:39 +0000

    update

--- The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
+++ The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
@@ -37,13 +37,18 @@ btnScrollTo.addEventListener('click', () => {
 });
 
 //*** PAGE NAVIGATION ***
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
-    console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  e.target.classList.contains('nav__link');
 });
 
 //***PRACTICE ***
