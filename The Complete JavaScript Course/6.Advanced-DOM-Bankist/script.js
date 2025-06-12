1ac2e95faa6955f01c27ede4d4d26dee128ff31a
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-06-12 20:36:14 +0000

    update

--- The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
+++ The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
@@ -54,17 +54,13 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
-
   //Guard clause
   if (!clicked) return;
-
   //remove active classes
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
-
   //Active Tab
   clicked.classList.add('operations__tab--active');
-
   //Active Content Area
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
