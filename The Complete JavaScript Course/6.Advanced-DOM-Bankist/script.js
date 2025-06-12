9311c8d40e9865b07a238b5215c3a43b1396267b
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-06-12 02:11:24 +0000

    update

--- The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
+++ The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { Component } = require('react');
-
 ///////////////////////////////////////
 // Modal window
 
@@ -49,7 +47,16 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 //Tabbed Component
 
-const tabs = document.querySelectorAll('operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+});
 
 //***PRACTICE ***
 
