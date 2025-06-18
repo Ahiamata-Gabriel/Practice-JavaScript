e135c9b307592828175fa25fb30b2473ed61a16c
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-06-18 05:29:42 +0000

    update

--- The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
+++ The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
@@ -112,6 +112,7 @@ window.addEventListener('scroll', function () {
 //observer.obser ve(section1);
 
 const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -123,7 +124,7 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: '-90px',
+  rootMargin: `-${navHeight}px`,
 });
 
 headerObserver.observe(header);
