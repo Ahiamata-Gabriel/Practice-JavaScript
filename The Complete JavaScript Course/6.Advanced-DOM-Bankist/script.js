2f7a17ba708bcb0687210d8f92a37e29749bb4c4
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-06-18 00:33:05 +0000

    update

--- The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
+++ The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
@@ -115,6 +115,8 @@ const header = document.querySelector('.header');
 
 const stickyNav = function (entries) {
   const [entry] = entries;
+  console.log(entry);
+
   nav.classList.add('sticky');
 };
 
@@ -122,3 +124,5 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
 });
+
+headerObserver.observe(header);
