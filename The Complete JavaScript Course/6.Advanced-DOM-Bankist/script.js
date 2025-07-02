8be88b639e9f01fd9bc193ccb2c670a52e6b7e4c
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-07-02 16:09:20 +0000

    update

--- The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
+++ The Complete JavaScript Course/6.Advanced-DOM-Bankist/script.js
@@ -51,20 +51,20 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 //Tabbed Component
 
-// tabsContainer.addEventListener('click', function (e) {
-//   const clicked = e.target.closest('.operations__tab');
-//   //Guard clause
-//   if (!clicked) return;
-//   //remove active classes
-//   tabs.forEach(t => t.classList.remove('operations__tab--active'));
-//   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
-//   //Active Tab
-//   clicked.classList.add('operations__tab--active');
-//   //Active Content Area
-//   document
-//     .querySelector(`.operations__content--${clicked.dataset.tab}`)
-//     .classList.add('operations__content--active');
-// });
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  //Guard clause
+  if (!clicked) return;
+  //remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  //Active Tab
+  clicked.classList.add('operations__tab--active');
+  //Active Content Area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 
 //Menu fade Animation
 const handleHover = function (e) {
