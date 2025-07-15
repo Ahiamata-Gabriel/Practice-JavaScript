238d3923db0737556461a8297ac6731980c8ac8f
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-07-15 05:37:43 +0000

    update

--- The Complete JavaScript Course/7.OOP/script.js
+++ The Complete JavaScript Course/7.OOP/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const gabby = new Person('Mawuli', 1991);
+
+console.log(gabby);
