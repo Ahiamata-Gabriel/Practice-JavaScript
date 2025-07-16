2598f361670898d5e178e2bb0597e4ce4219df0b
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-07-16 22:49:47 +0000

    update

--- The Complete JavaScript Course/7.OOP/script.js
+++ The Complete JavaScript Course/7.OOP/script.js
@@ -8,3 +8,18 @@ const Person = function (firstName, birthYear) {
 const gabby = new Person('Mawuli', 1991);
 
 console.log(gabby.__proto__ === Person.prototype);
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+
+  function accelarate() {
+    this.speed = speed + 10;
+    console.log(this.speed);
+  }
+
+  function brake() {
+    this.speed = speed - 5;
+    console.log(this.speed);
+  }
+};
