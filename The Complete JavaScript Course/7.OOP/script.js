4aa7c726dad656d0e7e1483efc1620165235834b
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-07-17 04:58:40 +0000

    update

--- The Complete JavaScript Course/7.OOP/script.js
+++ The Complete JavaScript Course/7.OOP/script.js
@@ -1,33 +1,48 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
 
-const gabby = new Person('Mawuli', 1991);
+// const gabby = new Person('Mawuli', 1991);
 
-console.log(gabby.__proto__ === Person.prototype);
+// console.log(gabby.__proto__ === Person.prototype);
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelarate = function () {
-  this.speed += 10;
-  console.log(this.speed);
-};
+// Car.prototype.accelarate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(this.speed);
-};
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
 
-const car1 = new Car('BMW', 120);
-const car2 = new Car('Mercedes', 95);
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Mercedes', 95);
 
-console.log(car1);
-console.log(car2);
+// console.log(car1);
+// console.log(car2);
 
-console.log(car1.brake());
+// console.log(car1.brake());
+
+class PersonCl {
+  constructor(name, birthYear) {
+    this.name = name;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2025 - this.birthYear);
+  }
+}
+
+const jonas = new PersonCl('jonas', 1991);
+
+console.log(jonas);
