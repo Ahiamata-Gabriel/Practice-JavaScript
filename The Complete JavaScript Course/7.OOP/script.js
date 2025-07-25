ae34bd000976f13c4e70da2f5e259cdc31a832d7
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-07-25 11:39:49 +0000

    update

--- The Complete JavaScript Course/7.OOP/script.js
+++ The Complete JavaScript Course/7.OOP/script.js
@@ -61,3 +61,7 @@ const Student = function (firstName, birthYear, course) {
   this.birthYear = birthYear;
   this.course = course;
 };
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
